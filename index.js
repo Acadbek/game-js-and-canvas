006c2809f79fc780ab7d419dbd4d14961dd1eca2
Asadbek Nosirjonov5566 <a.nosirjonov@uic.group>
2022-01-08 21:57:08 +0500

    urrraa's enemies are dead

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ const enemies = [];
 
 function spawnEnemies() {
     setInterval(() => {
-        const radius = 30;
+        const radius = Math.random() * (30 - 4) + 4;
 
         let x
         let y
@@ -113,8 +113,17 @@ function animate() {
     projectiles.forEach(projectile => {
         projectile.update();
     })
-    enemies.forEach(enemy => {
+    enemies.forEach((enemy, index) => {
         enemy.update();
+
+        projectiles.forEach((projectile, projectileIndex) => {
+            const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
+
+            if (dist - enemy.radius - projectile.radius < 1) {
+                enemies.splice(index, 1)
+                projectiles.splice(projectileIndex, 1)
+            }
+        })
     })
 }
 
