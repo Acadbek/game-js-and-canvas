d27c48ba33b73c9a4f2da3a254af5d5232acce74
Asadbek Nosirjonov5566 <a.nosirjonov@uic.group>
2022-01-08 20:55:21 +0500

    feat: func

--- index.js
+++ index.js
@@ -50,7 +50,6 @@ const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, 'blue');
-player.draw();
 
 
 const projectiles = [];
@@ -58,12 +57,19 @@ const projectiles = [];
 
 function animate() {
     requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    player.draw();
     projectiles.forEach(projectile => {
         projectile.update();
     })
 }
 
 addEventListener('click', (event) => {
-    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', { x: 1, y: 1 }))
+    const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2);
+    const velocity = {
+        x: Math.cos(angle),
+        y: Math.sin(angle)
+    }
+    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity))
 })
 animate();  
