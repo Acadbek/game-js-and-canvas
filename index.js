9ab8fd187a829f710476ae87eba44a895139c37c
Asadbek Nosirjonov5566 <a.nosirjonov@uic.group>
2022-01-09 02:25:50 +0500

    refactor: style

--- index.js
+++ index.js
@@ -114,30 +114,28 @@ const particles = [];
 
 function spawnEnemies() {
     setInterval(() => {
-        setInterval(() => {
-            const radius = Math.random() * (30 - 4) + 4;
-    
-            let x
-            let y
-    
-            if (Math.random() > 0.5) {
-                x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
-                y = Math.random() * canvas.height;
-            } else {
-                x = Math.random() * canvas.width
-                y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
-            }
-            const color = `hsl(${Math.random() * 360}, 50%, 50%)`;
-    
-            const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
-    
-            const velocity = {
-                x: Math.cos(angle) * 2,
-                y: Math.sin(angle) * 2
-            }
-            enemies.push(new Enemy(x, y, radius, color, velocity))
-        }, 1500);
-    }, 10000);
+        const radius = Math.random() * (30 - 4) + 4;
+
+        let x
+        let y
+
+        if (Math.random() > 0.5) {
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+            y = Math.random() * canvas.height;
+        } else {
+            x = Math.random() * canvas.width
+            y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+        }
+        const color = `hsl(${Math.random() * 360}, 50%, 50%)`;
+
+        const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+
+        const velocity = {
+            x: Math.cos(angle) * 2,
+            y: Math.sin(angle) * 2
+        }
+        enemies.push(new Enemy(x, y, radius, color, velocity))
+    }, 1000);
 }
 
 
