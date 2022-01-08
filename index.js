7f3598e8787db9af00b47a2c7ecfff87a6058a9e
Asadbek Nosirjonov5566 <a.nosirjonov@uic.group>
2022-01-09 02:16:09 +0500

    refactor: style

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ function spawnEnemies() {
             y: Math.sin(angle) * 2
         }
         enemies.push(new Enemy(x, y, radius, color, velocity))
-    }, Math.random() * 1000 + 1000);
+    }, Math.random() * 1000 + 10);
 }
 
 
