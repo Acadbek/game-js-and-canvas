33b0d573c0167500b5c7ce509543b930caa08b4a
Asadbek Nosirjonov5566 <a.nosirjonov@uic.group>
2022-01-09 00:41:14 +0500

    refactor: style

--- index.js
+++ index.js
@@ -130,11 +130,11 @@ function spawnEnemies() {
         const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
 
         const velocity = {
-            x: Math.cos(angle) - 5,
-            y: Math.sin(angle) - 5
+            x: Math.cos(angle),
+            y: Math.sin(angle)
         }
         enemies.push(new Enemy(x, y, radius, color, velocity))
-    }, 1000);
+    }, 1500);
 }
 
 
@@ -208,8 +208,8 @@ function animate() {
 addEventListener('click', (event) => {
     const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2);
     const velocity = {
-        x: Math.cos(angle) * 10,
-        y: Math.sin(angle) * 5
+        x: Math.cos(angle) * 6,
+        y: Math.sin(angle) * 6
     }
     projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'white', velocity))
 })
