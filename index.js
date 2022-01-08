2ee1c18cec64931fd6a574778d241077b525b32d
Asadbek Nosirjonov5566 <a.nosirjonov@uic.group>
2022-01-08 21:32:51 +0500

    refactor: enemies

--- index.js
+++ index.js
@@ -43,6 +43,28 @@ class Projectile {
     }
 }
 
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+    draw() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color;
+        c.fill()
+    }
+
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
+
 let body = document.querySelector('body');
 // body.style.backgroundColor = '#000';
 
@@ -50,9 +72,28 @@ const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, 'blue');
-
-
 const projectiles = [];
+const enemies = [];
+
+function spawnEnemies() {
+    setInterval(() => {
+        const radius = 30;
+        const x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+        const y = Math.random() * canvas.height;
+        const color = 'green';
+
+        const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+        enemies.push(new Enemy(x, y, radius, color, velocity))
+        // console.log(enemies)
+    }, 1000);
+}
+
+
 
 
 function animate() {
@@ -61,6 +102,9 @@ function animate() {
     player.draw();
     projectiles.forEach(projectile => {
         projectile.update();
+    })
+    enemies.forEach(enemy => {
+        enemy.update();
     })
 }
 
@@ -72,4 +116,5 @@ addEventListener('click', (event) => {
     }
     projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity))
 })
-animate();  
+animate();
+spawnEnemies()
