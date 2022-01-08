0b08679d1a63fc2de177f92b980dedb8ba4bccb0
Asadbek Nosirjonov5566 <a.nosirjonov@uic.group>
2022-01-08 19:35:44 +0500

    refactor: fillStyle

--- index.js
+++ index.js
@@ -11,8 +11,15 @@ class Player {
         this.radius = radius;
         this.color = color
     }
+
+    draw() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color;
+        c.fill()
+    }
 }
 
 const player = new Player(100, 100, 30, 'blue');
-
+player.draw();
 console.log(player);
